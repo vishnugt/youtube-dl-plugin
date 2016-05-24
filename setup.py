f17e595311a6be537b41b98aef6e929f7bf171f6
vishnu.g <vishnugt@hotmail.com>
2016-05-24 15:44:03 +0530

    Added in pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'youtube-dl-plugin',
+  packages = ['youtube-dl-plugin'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A plugin to ease your download through youtube-dl',
+  author = 'VishnuGt',
+  author_email = 'vishnugt@hotmail.com',
+  url = 'https://github.com/vishnugt/youtube-dl-plugin', # use the URL to the github repo
+  download_url = 'https://github.com/vishnugt/youtube-dl-plugin/tarball/1.0', # I'll explain this in a second
+  keywords = ['youtube-dl', 'youtube', 'downloader'], # arbitrary keywords
+  classifiers = [],
+)
