b90851483618905e3df5b6b79cee6c590483ef74
vishnu.g <vishnugt@hotmail.com>
2016-05-24 16:46:31 +0530

    Pushed to pypi, but hyphenated :(

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'youtube-dl-plugin',
   packages = ['youtube-dl-plugin'], # this must be the same as the name above
-  version = '0.1',
+  version = '1.6',
   description = 'A plugin to ease your download through youtube-dl',
   author = 'VishnuGt',
   author_email = 'vishnugt@hotmail.com',
   url = 'https://github.com/vishnugt/youtube-dl-plugin', # use the URL to the github repo
-  download_url = 'https://github.com/vishnugt/youtube-dl-plugin/tarball/1.0', # I'll explain this in a second
+  download_url = 'https://github.com/vishnugt/youtube-dl-plugin/tarball/1.6', # I'll explain this in a second
   keywords = ['youtube-dl', 'youtube', 'downloader'], # arbitrary keywords
   classifiers = [],
 )
---
+++ test.py
@@ -0,0 +1,4 @@
+foobar = __import__("youtube-dl-plugin")	
+from foobar import youtube_dl_plugin
+
+print("hello")
