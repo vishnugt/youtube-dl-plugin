9a481315cb8e1acd5bc67caee1d18336f98d269a
deferred <deferred@outlook.com>
2018-01-30 18:50:48 +0500

    Added a config file for user to input default download quality.
    Close #4

--- youtube-dl-plugin/youtube_dl_plugin.py
+++ youtube-dl-plugin/youtube_dl_plugin.py
@@ -3,23 +3,45 @@ import threading
 import os
 import subprocess
 import sys
-
-
 import pyperclip
+from configparser import ConfigParser
+
+
+def read_config_download_quality():
+    """Reads configuration file to retrieve download quality that should be used by youtube-dl"""
+    config = ConfigParser()
+    config.read('youtube_dl_plugin.conf')
+
+    if 'DownloadQuality' not in config['DEFAULT']:
+        download_quality = input("Input preferred youtube-dl download format, "
+                                 "leave blank for default value (refer to youtube-dl README.md): ")
+        config['DEFAULT']['DownloadQuality'] = download_quality
+        with open('youtube_dl_plugin.conf', 'w') as configfile:
+            config.write(configfile)
+
+    return config['DEFAULT']['DownloadQuality']
+
 
 def is_url_but_not_bitly(url):
     if url.startswith("https://www.youtube.com"):
         return True
     return False
 
+
 def print_to_stdout(clipboard_content):
     print(" !")
     tempstring = str(clipboard_content)
     pyperclip.copy('')
     #print "@"
+
+    download_quality = read_config_download_quality()
+    if download_quality != "":
+        tempstring = tempstring + " --format " + download_quality
+
     with open(os.devnull, "w") as fnull:
         subprocess.call("start youtube.exe "+tempstring, stdout = fnull, stderr = fnull, shell = True)
-    
+
+
 class ClipboardWatcher(threading.Thread):
     def __init__(self, predicate, callback, pause=5.):
         super(ClipboardWatcher, self).__init__()
@@ -40,6 +62,7 @@ class ClipboardWatcher(threading.Thread):
 
     def stop(self):
         self._stopping = True
+
 
 def main():
     watcher = ClipboardWatcher(is_url_but_not_bitly, 
