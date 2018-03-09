b857f21f3be68e8be6caf3f1ce948fbdea0d9fee
Vishnu Gt <vishnugt@hotmail.com>
2018-03-09 18:50:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'youtube-dl-plugin',
-  packages = ['youtube-dl-plugin'], # this must be the same as the name above
+  packages = ['youtube-dl-plugin'],
   version = '1.6',
   description = 'A plugin to ease your download through youtube-dl',
   author = 'VishnuGt',
   author_email = 'vishnugt@hotmail.com',
-  url = 'https://github.com/vishnugt/youtube-dl-plugin', # use the URL to the github repo
-  download_url = 'https://github.com/vishnugt/youtube-dl-plugin/tarball/1.6', # I'll explain this in a second
-  keywords = ['youtube-dl', 'youtube', 'downloader'], # arbitrary keywords
+  url = 'https://github.com/vishnugt/youtube-dl-plugin',
+  download_url = 'https://github.com/vishnugt/youtube-dl-plugin/tarball/1.6',
+  keywords = ['youtube-dl', 'youtube', 'downloader'],
   classifiers = [],
 )
